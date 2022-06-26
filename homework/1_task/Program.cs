e87262b5f4c422c39ceaefa8ed1287689789c677
Sam <kipriyanov.1991@mail.ru>
2022-06-26 15:26:36 +0300

    max_min

---
+++ homework/1_task/Program.cs
@@ -0,0 +1,13 @@
+﻿int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int max = a;
+int min = b;
+if (a < b)
+{
+    max = b;
+    min = a;
+}
+Console.Write("max = ");
+Console.WriteLine(max);
+Console.Write("min = ");
+Console.WriteLine(min);
