d29c27455699dd9b334964b6e263d451ea1304d3
Sam <kipriyanov.1991@mail.ru>
2022-06-26 15:40:46 +0300

    Максимальное число

---
+++ homework/2_task/Program.cs
@@ -0,0 +1,8 @@
+﻿int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+int max = a;
+if (b > a) max = b;
+if (c > b) max = c;
+Console.Write("max = ");
+Console.WriteLine(max);
