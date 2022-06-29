d23673d8b2a5315bcad4900cc87ca8f00dce414d
Sam <kipriyanov.1991@mail.ru>
2022-06-29 20:04:34 +0300

    Задача_4_день недели

--- 2_lesson/homework_2/task_4/Program.cs
+++ 2_lesson/homework_2/task_4/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿void TakeNum(int num)
+{
+    if (num == 6 || num == 7)
+    {
+        Console.WriteLine("Выходной");
+    }
+    else
+    {
+        Console.WriteLine("Рабочий");
+    }
+}
+// TakeNum ввести цифру от 1 до 7
+TakeNum(5);
+TakeNum(7);
