a34ddef9c451b03afbd4d5a219709bf4cf90a39a
Sam <kipriyanov.1991@mail.ru>
2022-06-29 19:46:37 +0300

    Убрать вторую цифру

---
+++ 2_lesson/homework_2/task_2/Program.cs
@@ -0,0 +1,10 @@
+﻿
+void FirstNum(int num)
+{
+    int num1 = num / 100;
+    int num2 = num % 10;
+    Console.WriteLine($"{num1}{num2}");
+}
+int SecondNum = new Random().Next(100, 999);
+Console.WriteLine(SecondNum);
+FirstNum(SecondNum);
