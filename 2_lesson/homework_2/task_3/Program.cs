ac93b727c3a721e851e8149d4253c669aa056e3e
Sam <kipriyanov.1991@mail.ru>
2022-06-29 21:12:47 +0300

    Задача_3 есть или нет 3 число_вывести

--- 2_lesson/homework_2/task_3/Program.cs
+++ 2_lesson/homework_2/task_3/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿void TakeNum(int num)
+{
+    if (num < 100)
+    {
+        Console.WriteLine("Третьей цифры нет");
+    }
+    else
+    {
+        int num1 = 0;
+        while (num > 999)
+        {
+            num = num / 10;
+        }
+        num1 = num % 10;
+        Console.WriteLine(num1);
+    }
+}
+TakeNum(762785);
