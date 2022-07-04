7453db207100982a944337e520e7fbb3e27750cf
Sam <kipriyanov.1991@mail.ru>
2022-07-04 22:15:28 +0300

    задача_1

---
+++ 4_lesson/homework_4/task_1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 1: Напишите цикл, который принимает на вход
+// два числа (A и B) и возводит число A в натуральную степень B.
+
+double ProdNum(int a, int b)
+{
+    double c = 1;
+
+    for (int i = 0; i < b; i++)
+        c = c * a;
+    return c;
+}
+Console.WriteLine(ProdNum(2, 4));
+Console.WriteLine(ProdNum(4, 9));
