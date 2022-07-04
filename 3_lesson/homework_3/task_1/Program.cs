658331823f6cf1925ede7e29a3cddf9b219848f1
Sam <kipriyanov.1991@mail.ru>
2022-07-04 20:55:58 +0300

    Задача_1

--- 3_lesson/homework_3/task_1/Program.cs
+++ 3_lesson/homework_3/task_1/Program.cs
@@ -4,4 +4,26 @@
 // 14212 -> нет
 // 23432 -> да
 
+void Pal(int num)
+{
+    int i = 0;
+    string numbers = num.ToString();
+    int size = numbers.Length - 1;
+    string flag = "Да";
+
+    while (i < size / 2)
+    {
+        if (numbers[i] != numbers[size - i])
+        {
+            flag = "Нет";
+            break;
+        }
+        i++;
+    }
+    Console.WriteLine($"{num} -> {flag}");
+}
+
+Pal(14212);
+Pal(123454321);
+
 
