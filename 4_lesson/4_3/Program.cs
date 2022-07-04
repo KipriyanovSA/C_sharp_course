850f131b11de669dcfa36d1e09c2d64fe1808b49
Sam <kipriyanov.1991@mail.ru>
2022-07-04 10:31:58 +0300

    Задача_3

---
+++ 4_lesson/4_3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 2: Напишите программу, которая
+// принимает на вход число N и выдаёт
+// произведение чисел от 1 до N
+
+int MultNum(int num)
+{
+    int mult = 1;
+    for (int i = 1; i <= num; i++)
+        mult *= i;
+    return mult;
+}
+Console.WriteLine(MultNum(4));
+Console.WriteLine(MultNum(6));
