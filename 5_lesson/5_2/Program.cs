ae765f8b164bbd91c57a0b85e1703589db5ad78c
Sam <kipriyanov.1991@mail.ru>
2022-07-13 15:38:47 +0300

    Задача 2 замена на противоположные

---
+++ 5_lesson/5_2/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 1: Напишите программу замена элементов массива:
+// положительные элементы замените на соответствующие
+// отрицательные, и наоборот.
+
+int[] NewArray(int size)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(-9, 10);
+    }
+    return arr;
+}
+
+void Print(int[] arr)
+{
+    int size = arr.Length;
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void ChangeNum(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = -arr[i];
+    }
+}
+
+int[] arr_1 = NewArray(10);
+Print(arr_1);
+ChangeNum(arr_1);
+Print(arr_1);
