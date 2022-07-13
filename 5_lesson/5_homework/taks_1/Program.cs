e3dc6649f406afb36217e3cf3868bdc64a5ef0b1
Sam <kipriyanov.1991@mail.ru>
2022-07-13 17:57:21 +0300

    Задача_1 к-во четных чисел в массиве

---
+++ 5_lesson/5_homework/taks_1/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int[] NewArray(int size)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(100, 999);
+    }
+    return arr;
+}
+
+void Print(int[] arr)
+{
+    int size = arr.Length;
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int Amount(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+            count++;
+    }
+    return count;
+}
+
+int[] arr_1 = NewArray(10);
+Print(arr_1);
+Console.WriteLine($"Количество четных чисел {Amount(arr_1)} ");
