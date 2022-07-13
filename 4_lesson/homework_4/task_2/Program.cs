7b87b6252657a873252d2ba8865cbf61c4c7b370
Sam <kipriyanov.1991@mail.ru>
2022-07-13 13:48:25 +0300

    Сумма цифр

---
+++ 4_lesson/homework_4/task_2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 2: Напишите программу, которая принимает на
+// вход число и выдаёт сумму цифр в числе.
+
+int Total(int num)
+{
+    int sum = 0;
+    while (num > 0)
+    {
+        sum = sum + num % 10;
+        num = num / 10;
+    }
+    return sum;
+}
+Console.WriteLine(Total(2366));
