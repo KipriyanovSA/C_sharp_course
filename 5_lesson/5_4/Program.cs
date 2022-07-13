06bca6298a22d38216dfc18dbebfa2a58a6a17bd
Sam <kipriyanov.1991@mail.ru>
2022-07-13 16:44:41 +0300

    Задача 4 к-во чисел в диапазоне в массиве

---
+++ 5_lesson/5_4/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 3: Задайте одномерный массив из 123 случайных чисел.
+// Найдите количество элементов массива, значения которых лежат в
+// отрезке [10,99].
+
+int[] NewArray(int size)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(1, 500);
+    }
+    return arr;
+}
+
+void Print(int[] arr)
+{
+    int size = arr.Length;
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int Amount(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] >= 10 && arr[i] <= 99)
+            count++;
+    }
+    return count;
+}
+
+int[] arr_1 = NewArray(123);
+Print(arr_1);
+Console.WriteLine(Amount(arr_1));
