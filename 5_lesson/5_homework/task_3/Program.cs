e28d4a3b3b3a61763fc4c9e8b2324f2c2aad670b
Sam <kipriyanov.1991@mail.ru>
2022-07-13 18:50:46 +0300

    Задача_3 макс- мин в массиве

---
+++ 5_lesson/5_homework/task_3/Program.cs
@@ -0,0 +1,43 @@
+﻿//  Задайте массив вещественных чисел. 
+//  Найдите разницу между максимальным и минимальным элементов массива.
+
+int[] NewArray(int size)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(1, 100);
+    }
+    return arr;
+}
+
+void Print(int[] arr)
+{
+    int size = arr.Length;
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int MaxMin(int[] arr)
+{
+    int max = 0;
+    int max_min = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > max) max = arr[i];
+    }
+    int min = max;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] < min) min = arr[i];
+    }
+    max_min = max - min;
+    return max_min;
+}
+
+int[] arr_1 = NewArray(5);
+Print(arr_1);
+Console.WriteLine(MaxMin(arr_1));
