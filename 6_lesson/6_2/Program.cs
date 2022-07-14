d8cdb46e2af7137b4d5efbff4f61497ea7bb08fd
Sam <kipriyanov.1991@mail.ru>
2022-07-14 17:08:35 +0300

    Перевести из 10 в 2 систему счисления

---
+++ 6_lesson/6_2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 2: Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+
+void Binary(int num)
+{
+    string Dec = "";
+    while (num > 0)
+    {
+        Dec = num % 2 + Dec;
+        num /= 2;
+    }
+    Console.WriteLine(Dec);
+}
+
+Binary(45);
