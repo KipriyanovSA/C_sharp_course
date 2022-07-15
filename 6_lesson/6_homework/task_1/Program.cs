b71735190c4dcb3b9bab6a09a395e209e8eeee1e
Sam <kipriyanov.1991@mail.ru>
2022-07-15 15:10:32 +0300

    Подсчет количества чисел больше 0 введенных пользователем

---
+++ 6_lesson/6_homework/task_1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 1: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223 -> 3
+
+int Numbers(int num)
+{
+    int count = 0;
+    for (int i = 1; i <= num; i++)
+    {
+        Console.Write($"Число {i}: ");
+        int n = int.Parse(Console.ReadLine());
+        if (n > 0) count++;
+    }
+    return count;
+}
+
+Console.Write("Сколько чисел нужно ввести?: ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine($"Количество чисел больше 0: {Numbers(n)}");
