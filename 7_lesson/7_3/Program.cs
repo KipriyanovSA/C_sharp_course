ac9089436a1bbb06f68889e1a09bd065bf6fa457
Sam <kipriyanov.1991@mail.ru>
2022-07-15 20:12:06 +0300

    Нахождение позиции элемента в двумерном массиве

---
+++ 7_lesson/7_3/Program.cs
@@ -0,0 +1,71 @@
+﻿// Задача 4: Задайте двумерный массив. Введите элемент, и найдите
+// первое его вхождение, выведите позиции по горизонтали и
+// вертикали, или напишите, что такого элемента нет.
+// Например, такой массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Введенный элемент 2, результат: [1, 4]
+// Введенный элемент 6, результат: такого элемента нет.
+
+void Print(int[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++)
+        {
+            Console.Write($" {arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] FillArray(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            arr[i, j] = new Random().Next(from, to);
+        }
+    }
+    return arr;
+}
+
+void FindPosOfNum(int[,] array, int num)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] == num)
+            {
+                Console.WriteLine($"[{i + 1}, {j + 1}]");
+                return;
+            }
+        }
+    }
+    Console.WriteLine("Не найдено");
+}
+
+
+
+
+Console.WriteLine("Введите количество строк: ");
+int row = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите количество столбцов: ");
+int column = int.Parse(Console.ReadLine());
+
+Console.WriteLine();
+
+int[,] arr_1 = FillArray(row, column, 1, 10);
+Print(arr_1);
+
+FindPosOfNum(arr_1, 2);
