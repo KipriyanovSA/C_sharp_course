3b87e9bd9e6e70389a03f0c4471ea92d5e0ea544
Sam <kipriyanov.1991@mail.ru>
2022-07-18 19:44:58 +0300

    Частотный словарь элементов двумерного массива

--- 8_lesson/8_3/Program.cs
+++ 8_lesson/8_3/Program.cs
@@ -1,1 +1,59 @@
-﻿
+﻿// Составить частотный словарь элементов
+// двумерного массива. Частотный словарь содержит
+// информацию о том, сколько раз встречается элемент
+// входных данных. Значения элементов массива 0..9
+
+void Print(int[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++)
+        {
+            Console.Write($" {arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] FillArray(int row, int column)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            arr[i, j] = new Random().Next(1, 10);
+        }
+    }
+    return arr;
+}
+
+int[] Vocabulary(int[,] arr)
+{
+    int[] array = new int[10];
+    foreach (int item in arr) array[item]++;
+    return array;
+}
+
+void PrintArray(int[] arr)
+{
+for (int i = 0; i < arr.Length; i++)
+    {
+        Console.WriteLine($"{i} {"встречается"} - {arr[i]} ");
+    }
+}
+
+Console.Write("Введите количество строк: ");
+int row = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int column = int.Parse(Console.ReadLine());
+int[,] arr_1 = FillArray(row, column);
+Print(arr_1);
+Console.WriteLine();
+int[] arr_2 = Vocabulary(arr_1);
+PrintArray(arr_2);
