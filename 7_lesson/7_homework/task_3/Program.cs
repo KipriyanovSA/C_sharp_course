5d9efe6b7d659410287f1c648b7853f5a8745977
Sam <kipriyanov.1991@mail.ru>
2022-07-18 10:15:44 +0300

    Недорешал 3 задачу

---
+++ 7_lesson/7_homework/task_3/Program.cs
@@ -0,0 +1,36 @@
+﻿//  Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+//  Например, задан массив:
+//  1 4 7 2
+//  5 9 2 3
+//  8 4 2 4
+//  Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+void Print(int[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++)
+        {
+            Console.Write($" {arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] FillArray(int row, int column)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            arr[i, j] = new Random().Next(1, 11);
+        }
+    }
+    return arr;
+}
